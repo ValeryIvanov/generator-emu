1299267f97816dfd0dd81030c16677cf24639822
Valery Ivanov <valeryi@proekspert.ee>
2016-05-19 09:36:34 +0300

    First example

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # generator-emu
+
+**How to run**
+
+1) Install Yeoman globally
+   > npm install -g yo
+2) Link emu generator to yo
+   > npm link
+3) Run emu generator (this should install node dependencies, if not, run 'npm install')
+   > yo emu
+4) Answer questions
+5) Profit!
+
+**If something fails**
+
+1) Update Node.js (download LTS version from nodejs.org and install)
+2) Update NPM
+   > npm install npm -g
+3) Update Yeoman
+   > npm update -g yo
+
+   or if that fails
+
+   > npm uninstall -g yo
+
+
+Read more about creating Yeoman generator
+http://yeoman.io/authoring/
+
--- generators/app/index.js
+++ generators/app/index.js
@@ -1,10 +1,49 @@
 var generators = require('yeoman-generator');
 
 module.exports = generators.Base.extend({
-  method1: function () {
-    console.log('method 1 just ran');
-  },
-  method2: function () {
-    console.log('method 2 just ran');
-  }
+    prompting: function () {
+        return this.prompt([
+            {
+                type: 'confirm',
+                name: 'auto_bpn',
+                message: 'Would you like to auto generate bpn?',
+            },
+            {
+                type: 'input',
+                name: 'bpn',
+                message: 'What should be customer\'s bpn?',
+                when: function (answers) {
+                    return !answers.auto_bpn;
+                }
+            },
+            {
+                type: 'confirm',
+                name: 'auto_partyId',
+                message: 'Would you like to auto generate partyId?',
+            },
+            {
+                type: 'input',
+                name: 'partyId',
+                message: 'What should be customer\'s partyId?',
+                when: function (answers) {
+                    return !answers.auto_partyId;
+                }
+            }
+        ]).then(function (answers) {
+            
+            // log answers
+            this.log('bpn', answers.bpn || (answers.bpn = '3000872146')); // TODO auto generate bpn
+            this.log('partyId', answers.partyId || (answers.partyId = 'A_2393809')); // TODO auto generate partyId
+            
+            // create configuration from answers to generate customer's data
+            this.fs.writeJSON('customers/' + answers.bpn + '.json', answers);
+
+            // create customer data files
+            this.fs.copyTpl(
+                this.templatePath('template1.xml'),
+                this.destinationPath('data/' + answers.bpn + '.xml'),
+                answers
+            );
+        }.bind(this));
+    }
 });
