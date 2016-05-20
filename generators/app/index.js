879cad8d298b9903902100375203aefb64da1d41
Valery Ivanov <valeri_ivanov35@hotmail.com>
2016-05-20 23:48:45 +0300

    more actions

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,49 +1,89 @@
+'use strict';
+
 var generators = require('yeoman-generator');
+var fs = require('fs');
+
+var newCustomerQuestions = [{
+    type: 'confirm',
+    name: 'auto_bpn',
+    message: 'Would you like to auto generate bpn?'
+}, {
+    type: 'input',
+    name: 'bpn',
+    message: 'What should be customer\'s bpn?',
+    when: function when(answers) {
+        return !answers.auto_bpn;
+    }
+}, {
+    type: 'confirm',
+    name: 'auto_partyId',
+    message: 'Would you like to auto generate partyId?'
+}, {
+    type: 'input',
+    name: 'partyId',
+    message: 'What should be customer\'s partyId?',
+    when: function when(answers) {
+        return !answers.auto_partyId;
+    }
+}];
+
+var existingCustomerQuestion = { type: 'input', name: 'file_name', message: 'Enter config file name (without file extension part)' };
+
+var newCustomerGenerationAction = 'Create new customer';
+var existingCustomerGenerationAction = 'Generate existing customer\'s data';
+var allCustomersGenerationAction = 'Generate all customers';
+
+var configDir = 'customers/';
+var dataDir = 'data/';
+
+function makeId(length, chars) {
+    var text = "";
+    var len = length || 5;
+    var possible = chars || "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for(var i = 0; i < len; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+}
 
 module.exports = generators.Base.extend({
-    prompting: function () {
-        return this.prompt([
-            {
-                type: 'confirm',
-                name: 'auto_bpn',
-                message: 'Would you like to auto generate bpn?',
-            },
-            {
-                type: 'input',
-                name: 'bpn',
-                message: 'What should be customer\'s bpn?',
-                when: function (answers) {
-                    return !answers.auto_bpn;
-                }
-            },
-            {
-                type: 'confirm',
-                name: 'auto_partyId',
-                message: 'Would you like to auto generate partyId?',
-            },
-            {
-                type: 'input',
-                name: 'partyId',
-                message: 'What should be customer\'s partyId?',
-                when: function (answers) {
-                    return !answers.auto_partyId;
-                }
-            }
-        ]).then(function (answers) {
-            
-            // log answers
-            this.log('bpn', answers.bpn || (answers.bpn = '3000872146')); // TODO auto generate bpn
-            this.log('partyId', answers.partyId || (answers.partyId = 'A_2393809')); // TODO auto generate partyId
-            
-            // create configuration from answers to generate customer's data
-            this.fs.writeJSON('customers/' + answers.bpn + '.json', answers);
+    prompting: function prompting() {
+        return this.prompt([{
+            type: 'list',
+            name: 'action',
+            message: 'What do you want to do?',
+            choices: [newCustomerGenerationAction, existingCustomerGenerationAction, allCustomersGenerationAction]
+        }]).then(function (answers) {
+            var self = this;
+            if (answers.action === newCustomerGenerationAction) {
+                self.prompt(newCustomerQuestions).then(function (answers) {
+                    if (answers.auto_bpn) {
+                        answers.bpn = makeId();
+                    }
+                    if (answers.auto_partyId) {
+                        answers.partyId = makeId();
+                    }
+                    self.fs.copyTpl(self.templatePath(), self.destinationPath(dataDir + answers.bpn), answers);
+                    self.fs.writeJSON(configDir + answers.bpn + '.json', answers);
+                });
 
-            // create customer data files
-            this.fs.copyTpl(
-                this.templatePath('template1.xml'),
-                this.destinationPath('data/' + answers.bpn + '.xml'),
-                answers
-            );
+            } else if (answers.action === existingCustomerGenerationAction) {
+                self.prompt(existingCustomerQuestion).then(function (answers) {
+                    var configObj = self.fs.readJSON(configDir + answers.file_name + '.json');
+                    self.fs.copyTpl(self.templatePath(), self.destinationPath(dataDir + configObj.bpn), configObj);
+                });
+            } else if (answers.action === allCustomersGenerationAction) {
+                fs.readdir(configDir, function(err, filenames) {
+                    filenames.forEach(function(filename) {
+                        fs.readFile(configDir + filename, 'utf-8', function(err, configObj) {
+                            configObj = JSON.parse(configObj);
+                            self.fs.copyTpl(self.templatePath(), self.destinationPath(dataDir + configObj.bpn), configObj);
+                        });
+                    });
+                });
+            } else {
+                self.log('This action is not yet implmented, sorry :(');
+            }
         }.bind(this));
     }
 });
